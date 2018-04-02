debb68bc95b7f74a6325106c6867a222e05d17c1
Deist <code@getbraintree.com>
2018-04-02 10:11:11 -0700

    Use modules for day constants

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,15 +1,37 @@
 class Day < ActiveRecord::Base
   belongs_to :cycle
 
+  module Temperatures
+    NormalRange = (96..100).step(0.1)
+  end
+
+  module Characteristics
+    Slippery = "s"
+    Tacky = "t"
+    None = "n"
+  end
+
+  module Sensations
+    Wet = "w"
+    Moist = "m"
+    Dry = "d"
+  end
+
+  module Cervix
+    Soft = "s"
+    Hard = "h"
+  end
+
   CHARACTERISTICS_FERTILITY_SCORE = {
-    "s" => 3,
-    "t" => 2,
-    "n" => 1,
+    Day::Characteristics::Slippery => 3,
+    Day::Characteristics::Tacky => 2,
+    Day::Characteristics::None => 1,
   }
+
   SENSATIONS_FERTILITY_SCORE = {
-    "w" => 3,
-    "m" => 2,
-    "d" => 1,
+    Day::Sensations::Wet => 3,
+    Day::Sensations::Moist => 2,
+    Day::Sensations::Dry => 1,
   }
 
   def bleeding?
