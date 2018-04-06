870139b89427be013b1e5299fc9146521faebcb0
Deist <code@getbraintree.com>
2018-04-06 09:05:56 -0700

    Add more graphs

--- app/helpers/graph_helper.rb
+++ app/helpers/graph_helper.rb
@@ -1,18 +1,78 @@
 module GraphHelper
   def temperatures_by_day
-    line_chart @days.map {|day| [day.number, day.temp] }, 
-      title: {text: ''},
-      yAxis: {
-        min: 95,
-        allowDecimals: true,
-        title: {
-          text: 'Temperature'
+    temperature_data = @days.map {|day| [day.number, day.temp] }
+    line_chart [
+      {
+        name: "Temperatures",
+        data: temperature_data,
+      },
+    ],
+    library: {
+      title: "Temperatures",
+      subtitle: "",
+      vAxis: {
+        viewWindowMode: 'explicit',
+          viewWindow: {
+          max:100,
+          min: 95,
         }
       },
-      xAxis: {
-        title: {
-          text: 'Cycle day number'
-        }
+    }
+  end
+
+  def other_chars_by_day
+    chart_data = [:irritability, :sensitivity, :drinks, :hours_sleep].map do |characteristic|
+      data = @days.map {|day| [day.number, day.send(characteristic)] }
+      {
+        name: characteristic.to_s,
+        data: data,
       }
+    end
+    line_chart chart_data,
+    library: {
+      title: "Other characteristics",
+      subtitle: "",
+      legend: "right",
+      hAxis: {
+        title: 'Cycle Day',
+        viewWindowMode: 'explicit',
+          viewWindow: {
+          max: @days.last.number,
+          min: @days.first.number,
+        }
+      },
+    }
+  end
+
+  def weight_by_day
+    # chart_data = [:irritability, :sensitivity, :drinks, :hours_sleep].map do |characteristic|
+      characteristic = :weight
+      data = @days.map {|day| [day.number, day.send(characteristic)] }
+      chart_data = [
+        {
+        name: characteristic.to_s,
+        data: data,
+      },
+      ]
+    # end
+    line_chart chart_data,
+    library: {
+      title: "Weight",
+      subtitle: "",
+      vAxis: {
+        viewWindowMode: 'explicit',
+          viewWindow: {
+          max: 150,
+          min: 130,
+        }
+      },
+      hAxis: {
+        viewWindowMode: 'explicit',
+          viewWindow: {
+          max: @days.last.number,
+          min: @days.first.number,
+        }
+      },
+    }
   end
 end
